f95bfd294989266da554d0d4d5cbb595f6ca0c83
Ede Mounika <edemounika99@gmail.com>
2023-09-07 18:50:40 +0530

    v-24 Streams and buffer

---
+++ streams-Buffer.js
@@ -0,0 +1,9 @@
+const buffer = new Buffer.from ("Mounika");
+
+
+// buffer.write("Code");
+// console.log(buffer.toString());
+console.log(buffer);
+console.log(buffer.toJSON());
+
+// Buffers has limited Memory
